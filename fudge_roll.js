c2ab1b2a71142e80539d3ae3cbc14706faee11f4
WGAVermeer <90707235+WGAVermeer@users.noreply.github.com>
2026-03-18 18:24:02 +0100

    updated fudge roll for arbitrary dice rolls (also fixed it)

--- fudge_roll.js
+++ fudge_roll.js
@@ -1,26 +1,28 @@
 new Dialog({
-  title: "Enter d20 Result",
-  content: 
-    <form>
-      <div class="form-group">
-        <label>d20 Result:</label>
-        <input type="number" name="die" min="1" max="20" value="20"/>
+  title: "Charon's Dice Fudger!",
+  content:` 
+    <form style="display: flex; justify-content: center">
+      <div class="form-group" style="display: flex; align-items:center, gap:5px;">
+        <label style="margin-right: 8px; flex: 0 0 auto">Enter wanted dice result:</label>
+        <input type="number" style="width: 30px; flex: 0 0 auto" name="dieResult" min="1" value="20"/>
+        <span style="width: 10px; flex: 0 0 auto;">d</span>
+        <input type="number" style="flex: 0 0 auto; width: 30px;"name="dieSize" min="1" value="20" />
       </div>
     </form>
-  ,
+  `,
   buttons: {
     roll: {
       label: "Roll",
       callback: async (html) => {
-        const value = Number(html.find('[name="die"]').val());
+        const size = Number(html.find('[name="dieSize"]').val());
+        const value = Number(html.find('[name="dieResult"]').val());
 
-        if (value < 1  value > 20 
- isNaN(value)) {
-          ui.notifications.error("Invalid d20 result (must be 1–20)");
+        if (value < 1 || value > size || isNaN(value)) {
+          ui.notifications.error(`Invalid d${size} result (must be 1–${size})`);
           return;
         }
 
-        const roll = await new Roll("1d20").evaluate({ async: true });
+        const roll = await new Roll(`1d${size}`).evaluate({ async: true });
 
         // Force the die result properly
         roll.terms[0].results[0].result = value;
